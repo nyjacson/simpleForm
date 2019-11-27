0ed49b834089e07e62a59974ee489a2972f18ce3
Yosuke, Tsutsumi | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2019-11-27 14:16:09 +0900

    updat dup filter and remote warnings

--- src/selector/userInputSelector.js
+++ src/selector/userInputSelector.js
@@ -7,16 +7,20 @@ const getSelectedRestaurant = state => state.userInput.restaurant || null;
 export const getRestrantList = createSelector(
   [getDishes, getSelectedMeal],
   (dishes, selectedMeal) => {
-    return dishes.items.filter(d => {
-      return d.availableMeals.indexOf(selectedMeal) >= 0 || false
+    let list = []
+    dishes.items.map(d => {
+      if (d.availableMeals.indexOf(selectedMeal) >= 0) {
+        list.push(d.restaurant);
+      }
     })
+    // remove duplication
+    return list.filter((x, i, self) => self.indexOf(x) === i);
   }
 )
 
 export const getDishList = createSelector(
   [getDishes, getSelectedMeal, getSelectedRestaurant],
   (dishes, selectedMeal, restaurant) => {
-    console.log(dishes, selectedMeal, restaurant)
     return dishes.items.filter(d => {
       return d.availableMeals.indexOf(selectedMeal) >= 0 && d.restaurant === restaurant
     })
